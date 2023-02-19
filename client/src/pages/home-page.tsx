fcf8e6fdf0b46b510e94df7e0baf7f0cab326bb8
ViktorijaVer <viktorija.verbickaite@gmail.com>
2023-02-19 22:19:27 +0200

    feat: home-page.tsx

--- client/src/pages/home-page.tsx
+++ client/src/pages/home-page.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Box, Typography } from '@mui/material'
 
 const HomePage = () => (
-  <div>HomePage</div>
+  <Box sx={{ textAlign: 'center' }}>
+    <Typography variant="h1">React MUI typescript template.</Typography>
+    <Typography variant="h2">This content is in:</Typography>
+    <Typography variant="h3">./src/pages/home-page.tsx</Typography>
+  </Box>
 );
 
 export default HomePage;
